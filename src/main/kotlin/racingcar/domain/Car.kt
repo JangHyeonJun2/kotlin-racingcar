8a3266ece44d5b83151a0accca410355c33abfc8
JangHyeonJun2 <epfvkdlxj@naver.com>
2024-01-21 23:14:43 +0900

    add: car 도메인로직 생성

---
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -0,0 +1,17 @@
+package racingcar.domain
+
+class Car(
+    val name:String,
+    var position: Int
+) {
+    init {
+        require(name.length in 1..5) { "이름의 길이는 1 이상 5 이하입니다." }
+        require(position >= 0) { "위치값은 0 이상이어야 합니다" }
+    }
+
+    fun move(power: Int) {
+        if (power in 4..9) {
+            position += 1
+        }
+    }
+}
